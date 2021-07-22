8aa70418b74f9fe5d69eb6024d476afc0ee0ad85
gary <gary.white@lodgit.com>
2021-07-22 17:23:41 +0200

    buttons connected

--- code.js
+++ code.js
@@ -3,11 +3,12 @@
 var rows = 24;
 var cols = 24;
 
-var playing = true;
+var playing = true; // track if game is running
 
 // init
 function initialize(){
     createTable();
+    setupControlButtons();
 }
 
 // lay out the board
@@ -41,6 +42,44 @@ function cellClickHandler(){
         this.setAttribute("class", "live");
     }
 }
+
+function setupControlButtons() {
+    // button to start
+    var startButton = document.getElementById("start");
+    startButton.onclick = startButtonHandler;
+
+    // button to clear
+    var clearButton = document.getElementById("clear");
+    clearButton.onclick = clearButtonHandler;
+}
+
+// clear the grid
+function clearButtonHandler(){
+    console.log("Clear the game: stop playing, clear the grid");
+    playing = false;
+    var startButton = document.getElementById("start");
+    startButton.innerHTML = "start";
+}
+
+// start/pause/continue the game
+function startButtonHandler() {
+    if (playing) {
+        console.log("Pause the game");
+        playing = false;
+        this.innerHTML = "continue";
+    } else {
+        console.log("Continue the game");
+        playing = true;
+        this.innerHTML = "pause";
+        play();
+    }
+}
+
+// Run the game
+function play() {
+    console.log("Play the game");
+}
+
 
 // Start everything
 window.onload = initialize;
