0d7b1555bfa3c9a453ef40b1cb19bf360c918033
gary <gary.white@lodgit.com>
2021-08-04 16:53:54 +0200

    random button added

--- code.js
+++ code.js
@@ -98,6 +98,25 @@ function setupControlButtons() {
     // button to clear
     var clearButton = document.getElementById("clear");
     clearButton.onclick = clearButtonHandler;
+
+    // button to set random initial state
+    var randomButton = document.getElementById("random");
+    randomButton.onclick = randomButtonHandler;
+}
+
+function randomButtonHandler() {
+    if (playing) return;
+    clearButtonHandler();
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < cols; j++) {
+            var isLive = Math.round(Math.random());
+            if (isLive == 1) {
+                var cell = document.getElementById(i + "_" + j);
+                cell.setAttribute("class", "live");
+                grid[i][j] = 1;
+            }
+        }
+    }
 }
 
 function clearButtonHandler() {
